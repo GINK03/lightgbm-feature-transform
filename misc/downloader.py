ddc24791ba97947e6a585397aa10143224fe108c
nardtree <gim.kobayashi@gmail.com>
2017-11-05 01:14:47 +0900

    update

--- misc/downloader.py
+++ misc/downloader.py
@@ -5,3 +5,5 @@ import sys
 if '--specified' in sys.argv:
   os.system('mc cp hatsuzuki/lightgbm-shrinkage-dataset/xgb_format .')
   os.system('mc cp hatsuzuki/lightgbm-shrinkage-dataset/term_index.pkl .')
+  os.system('mc cp hatsuzuki/lightgbm-shrinkage-dataset/LightGBM_model.txt .')
+  os.system('mc cp hatsuzuki/lightgbm-shrinkage-dataset/gbdt_prediction.cpp .')
